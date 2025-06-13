0d0f3f6633fe09181b1c139b3cde0b81a7a571fe
dulakshaaa <dulaksharajapaksha4@gmail.com>
2025-06-13 08:03:18 +0530

    Agripro to Farm Visit

--- c-home.php
+++ c-home.php
@@ -184,7 +184,7 @@ if (isset($_GET['export']) && $_GET['export'] == 'csv') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AgriPro | Visit Management</title>
+    <title>Farm Visit | Visit Management</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="./css/c-home.css">
